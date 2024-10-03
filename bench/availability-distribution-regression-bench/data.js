0f86907bfadd7d6bf44617ad20af243770419490
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 19:27:47 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 3313163485c4d4946ea97477407107796469f1c4

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727979111400,
+  "lastUpdate": 1727983667002,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -28105,6 +28105,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.008850444946666744,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "niklasadolfsson1@gmail.com",
+            "name": "Niklas Adolfsson",
+            "username": "niklasad1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3313163485c4d4946ea97477407107796469f1c4",
+          "message": "rpc v2: backpressure chainHead_v1_storage (#5741)\n\nClose https://github.com/paritytech/polkadot-sdk/issues/5589\n\nThis PR makes it possible for `rpc_v2::Storage::query_iter_paginated` to\nbe \"backpressured\" which is achieved by having a channel where the\nresult is sent back and when this channel is \"full\" we pause the\niteration.\n\nThe chainHead_follow has an internal channel which doesn't represent the\nactual connection and that is set to a very small number (16). Recall\nthat the JSON-RPC server has a dedicate buffer for each connection by\ndefault of 64.\n\n#### Notes\n\n- Because `archive_storage` also depends on\n`rpc_v2::Storage::query_iter_paginated` I had to tweak the method to\nsupport limits as well. The reason is that archive_storage won't get\nbackpressured properly because it's not an subscription. (it would much\neasier if it would be a subscription in rpc v2 spec because nothing\nagainst querying huge amount storage keys)\n- `query_iter_paginated` doesn't necessarily return the storage \"in\norder\" such as\n- `query_iter_paginated(vec![(\"key1\", hash), (\"key2\", value)], ...)`\ncould return them in arbitrary order because it's wrapped in\nFuturesUnordered but I could change that if we want to process it\ninorder (it's slower)\n- there is technically no limit on the number of storage queries in each\n`chainHead_v1_storage call` rather than the rpc max message limit which\n10MB and only allowed to max 16 calls `chainHead_v1_x` concurrently\n(this should be fine)\n\n#### Benchmarks using subxt on localhost\n\n- Iterate over 10 accounts on westend-dev -> ~2-3x faster \n- Fetch 1024 storage values (i.e, not descedant values) -> ~50x faster\n- Fetch 1024 descendant values -> ~500x faster\n\nThe reason for this is because as Josep explained in the issue is that\none is only allowed query five storage items per call and clients has\nmake lots of calls to drive it forward..\n\n---------\n\nCo-authored-by: command-bot <>\nCo-authored-by: James Wilson <james@jsdw.me>",
+          "timestamp": "2024-10-03T18:04:27Z",
+          "tree_id": "22a18815e1ae928d0fc5047f2cc776369ffc0a79",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/3313163485c4d4946ea97477407107796469f1c4"
+        },
+        "date": 1727983648071,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.016122229673333332,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.17970866715999997,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.02369593043333334,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.008814077360000077,
             "unit": "seconds"
           }
         ]
