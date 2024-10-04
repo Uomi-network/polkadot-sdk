563595891b2e7eeafe8f3b624e96c220096230c8
github-action-benchmark <github@users.noreply.github.com>
2024-10-04 11:07:19 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 111b2447a0ce9122a31ad67c4a8ad1479db159a0

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728036998919,
+  "lastUpdate": 1728040039338,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -28375,6 +28375,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.008522200093333415,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "claravanstaden64@gmail.com",
+            "name": "Clara van Staden",
+            "username": "claravanstaden"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "111b2447a0ce9122a31ad67c4a8ad1479db159a0",
+          "message": "Prevents EthereumBlobExporter from consuming dest when returning NotApplicable (#5789)\n\n# Description\n\nThe EthereumBlobExporter consumes the `dest` parameter when the\ndestination is not `Here`. Subsequent exporters will receive a `None`\nvalue for the destination instead of the original destination value,\nwhich is incorrect.\n \nCloses #5788\n\n## Integration\n\nMinor fix related to the exporter behaviour.\n\n## Review Notes\n\nVerified that tests\n`exporter_validate_with_invalid_dest_does_not_alter_destination` and\n`exporter_validate_with_invalid_universal_source_does_not_alter_universal_source`\nfail without the fix in the exporter.\n\n---------\n\nCo-authored-by: Adrian Catangiu <adrian@parity.io>",
+          "timestamp": "2024-10-04T09:55:18Z",
+          "tree_id": "6a43756baa6156ecf648c239ce075ce3bd47486a",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/111b2447a0ce9122a31ad67c4a8ad1479db159a0"
+        },
+        "date": 1728040020499,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.18105009502666658,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.016723298486666663,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.02393927818666667,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.00885356258666674,
             "unit": "seconds"
           }
         ]
