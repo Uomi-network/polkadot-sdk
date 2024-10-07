3fa3c642b7050bf94162fb3ec8940c1c9277b340
github-action-benchmark <github@users.noreply.github.com>
2024-10-07 11:04:41 +0000

    ___Updated docs

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728294095851,
+  "lastUpdate": 1728299080974,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -13679,6 +13679,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "statement-distribution",
             "value": 0.03553543997800002,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60601340+lexnv@users.noreply.github.com",
+            "name": "Alexandru Vasile",
+            "username": "lexnv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d66a5a41086718af03a272fd62b779eb8b81a74f",
+          "message": "chainHead: Track reported blocks to capture notification gaps (#5856)\n\nThere are cases during warp sync or re-orgs, where we receive a\nnotification with a block parent that was not reported in the past. This\nPR extends the tracking state to catch those cases and report a `Stop`\nevent to the user.\n\nThis PR adds a new state to the RPC-v2 chainHead to track which blocks\nhave been reported.\n\nIn the past we relied on the pinning mechanism to provide us details if\na block is pinned or not.\nHowever, the pinning state keeps the minimal information around for\npinning. Therefore, unpinning a block will cause the state to disappear.\n\nCloses: https://github.com/paritytech/polkadot-sdk/issues/5761\n\n---------\n\nSigned-off-by: Alexandru Vasile <alexandru.vasile@parity.io>\nCo-authored-by: Sebastian Kunert <skunert49@gmail.com>",
+          "timestamp": "2024-10-07T09:46:56Z",
+          "tree_id": "fc1e57aed5a22fdf664d11a51cb15f06b6dbb45c",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/d66a5a41086718af03a272fd62b779eb8b81a74f"
+        },
+        "date": 1728299062174,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.93799999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.044371911731999926,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.035338421395999985,
             "unit": "seconds"
           }
         ]
