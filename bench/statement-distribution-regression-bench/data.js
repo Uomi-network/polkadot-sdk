483305eaad628c560a26f1eb821da6691c2e6d77
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 09:52:28 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 4a70b2cffb23db148a9af50cfbf13c4655dbaf7b

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728549886609,
+  "lastUpdate": 1728553948544,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -14471,6 +14471,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "statement-distribution",
             "value": 0.035434155079999995,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "franciscoaguirreperez@gmail.com",
+            "name": "Francisco Aguirre",
+            "username": "franciscoaguirre"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4a70b2cffb23db148a9af50cfbf13c4655dbaf7b",
+          "message": "Remove redundant XCMs from dry run's forwarded xcms (#5913)\n\n# Description\n\nThis PR addresses\nhttps://github.com/paritytech/polkadot-sdk/issues/5878.\n\nAfter dry running an xcm on asset hub, we had redundant xcms showing up\nin the `forwarded_xcms` field of the dry run effects returned.\nThese were caused by two things:\n- The `UpwardMessageSender` router always added an element even if there\nwere no messages.\n- The two routers on asset hub westend related to bridging (to rococo\nand sepolia) getting the message from their queues when their queues is\nactually the same xcmp queue that was already contemplated.\n\nIn order to fix this, we check for no messages in UMP and clear the\nimplementation of `InspectMessageQueues` for these bridging routers.\nKeep in mind that the bridged message is still sent, as normal via the\nxcmp-queue to Bridge Hub.\nTo keep on dry-running the journey of the message, the next hop to\ndry-run is Bridge Hub.\nThat'll be tackled in a different PR.\n\nAdded a test in `bridge-hub-westend-integration-tests` and\n`bridge-hub-rococo-integration-tests` that show that dry-running a\ntransfer across the bridge from asset hub results in one and only one\nmessage sent to bridge hub.\n\n## TODO\n- [x] Functionality\n- [x] Test\n\n---------\n\nCo-authored-by: command-bot <>",
+          "timestamp": "2024-10-10T08:46:49Z",
+          "tree_id": "0a6f1d88c0d50756243a6f902f4c4339ca8a34c8",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/4a70b2cffb23db148a9af50cfbf13c4655dbaf7b"
+        },
+        "date": 1728553927611,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.93199999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04441562796199995,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.03523675500199999,
             "unit": "seconds"
           }
         ]
