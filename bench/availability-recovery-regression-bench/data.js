0b5454855753fda3610a898c84c8fc655ad44387
github-action-benchmark <github@users.noreply.github.com>
2024-10-11 18:20:24 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for c0b734336a68b6f48ac70a9b9507d8ddb9fed57e

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728651536357,
+  "lastUpdate": 1728670824207,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -24059,6 +24059,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-recovery",
             "value": 11.971792218966666,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1177472+mordamax@users.noreply.github.com",
+            "name": "Maksym H",
+            "username": "mordamax"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c0b734336a68b6f48ac70a9b9507d8ddb9fed57e",
+          "message": "/cmd: Improved devx of benching many pallets simultaneously (#6007)\n\n### Improved devx of running many pallets simultaneously\n\nChanges to /cmd:\n- Replace (flip) `--continue-on-fail` with `--fail-fast`, but only for\n`bench`. This makes all pallets/runtimes run non-stop by default, as it\nwas primary use-case during tests\n- The list of successful/failed pallets was hard to find within tons of\nlogs, so decided to write only needed logs in a file, and output as a\nsummary in the workflow and in the comment\n- Side fix: updated `tasks_example` to `pallet_example_tasks` to make\ncompliant with standard naming\n\n<img width=\"1006\" alt=\"image\"\nsrc=\"https://github.com/user-attachments/assets/14896041-7018-4a0d-92b7-4508e81913c2\">\n\n+ added command results to workflow summary:\n<img width=\"1275\" alt=\"image\"\nsrc=\"https://github.com/user-attachments/assets/b4a8afdb-dc9f-4ff9-9720-28a88956035f\">\n\n---------\n\nCo-authored-by: GitHub Action <action@github.com>",
+          "timestamp": "2024-10-11T17:20:57Z",
+          "tree_id": "1f74571a0c13e816d7b05ed444fc3dd4cdcbb738",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/c0b734336a68b6f48ac70a9b9507d8ddb9fed57e"
+        },
+        "date": 1728670803482,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.874284772733334,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.23293997983333337,
             "unit": "seconds"
           }
         ]
